140292a0803ab85ceecb19b99c32bed290b2a071
Hugo Seixas Antunes <hugo.seixas.antunes@zlabels.de>
2019-09-11 17:32:07 +0200

    only super users can see the iban

--- spacebase/user/admin.py
+++ spacebase/user/admin.py
@@ -7,7 +7,18 @@ from user.forms import UserForm
 class UserAdmin(admin.ModelAdmin):
     form = UserForm
 
-    list_display = ['first_name', 'last_name', 'iban']
-    search_fields = ['first_name', 'last_name', 'iban']
+    search_fields = ['first_name', 'last_name']
+
+    def hide_iban(self, obj):
+        return f"---{obj.iban[-4:]}"
+    hide_iban.short_description = 'iban'
+
+    def get_list_display(self, request):
+        display_list = ('first_name', 'last_name')
+        if request.user.is_superuser:
+            return display_list + ('iban',)
+        else:
+            return display_list + ('hide_iban',)
+
 
 admin.site.register(User, UserAdmin)
--- spacebase/user/tests.py
+++ spacebase/user/tests.py
@@ -1,3 +1,49 @@
+from django.contrib.admin.sites import AdminSite
 from django.test import TestCase
 
-# Create your tests here.
+from user.models import User as ClientUser
+from user.admin import UserAdmin
+
+
+class MockRequest:
+    pass
+
+
+class MockSuperUser:
+
+    @property
+    def is_superuser(self):
+        return True
+
+
+class MockUser:
+
+    @property
+    def is_superuser(self):
+        return False
+
+
+request = MockRequest()
+request.user = MockSuperUser()
+
+
+class ModelAdminTests(TestCase):
+
+    def setUp(self):
+        self.user = ClientUser.objects.create(
+            first_name='hugo',
+            last_name='antunes',
+            iban='12345678',
+        )
+        self.site = AdminSite()
+
+    def test_hide_iban_should_mask_full_user_iban(self):
+        admin = UserAdmin(ClientUser, self.site)
+        self.assertEqual(admin.hide_iban(self.user), '---5678')
+
+    def test_super_users_should_see_iban_field_instead_hide_iban(self):
+        admin = UserAdmin(ClientUser, self.site)
+        self.assertEqual(list(admin.get_list_display(request)), ['first_name', 'last_name', 'iban'])
+
+        request.user = MockUser()
+        self.assertEqual(list(admin.get_list_display(request)), ['first_name', 'last_name', 'hide_iban'])
